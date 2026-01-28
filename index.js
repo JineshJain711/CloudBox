dc26e6a96e884dab2530c48f246a2b2e0a773805
Jinesh jain <jineshjain711@gmail.com>
2026-01-29 00:27:44 +0530

    Refactor index.js to include CORS and dotenvmain file is updated
    Added CORS middleware and dotenv configuration. Improved server initialization and organization.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-const {dbConnect} = require("./config/db")
-const PORT = process.env.PORT;
+const { dbConnect } = require("./config/db");
+
 const authRouter = require("./routes/auth");
-const fileRouter = require("./routes/file")
-app.use(express.json())
-app.listen(PORT,()=>{
-    console.log(`App is Listning on Port ${PORT}`)
-})
+const fileRouter = require("./routes/file");
+
+// ================= MIDDLEWARE =================
+app.use(cors()); 
+app.use(express.json());
+
+// ================= ROUTES =================
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/file", fileRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is Running");
+});
+
 dbConnect();
 
-
-app.use("/api/v1/auth",authRouter);
-app.use("/api/v1/file",fileRouter)
-
-
-app.get("/",(req,res)=>{
-    res.send("Server is Running")
-})
+// ================= SERVER =================
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is Listening on Port ${PORT}`);
+});
